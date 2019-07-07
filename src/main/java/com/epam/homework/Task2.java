aab0d893793f70d2656b4f42870e2d022ececc70
mrPTqp <mrPTqp@gmail.com>
2019-07-07 20:23:17 +0300

    Solve task 2

--- src/main/java/com/epam/homework/Task2.java
+++ src/main/java/com/epam/homework/Task2.java
@@ -1,5 +1,9 @@
 package com.epam.homework;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Task2 {
 
     /**
@@ -38,11 +42,64 @@ public class Task2 {
      * (32): Солнце садится за горы лесистые.
      * (33): В царстве вечернем зеленой весны.
      */
-    public static void main(String[] args) {
-        // TODO реализация
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        // TODO foreach($current : $result) {
-        // TODO     System.out.println("(" + current.length() + "): " + current);
-        // TODO }
+        String numberOfLinesString = br.readLine();
+        if (isExitCommand(numberOfLinesString)) return;
+
+        int numberOfLines = Integer.valueOf(numberOfLinesString);
+        String[] inputLinesArr = new String[numberOfLines];
+
+        for (int i = 0; i < numberOfLines; i++) {
+            String currentLine = br.readLine();
+            if (isExitCommand(currentLine)) return;
+            inputLinesArr[i] = currentLine;
+        }
+
+        String[] resultArr = insertSort(inputLinesArr);
+        for (String current : resultArr
+        ) {
+            System.out.println("(" + current.length() + "): " + current);
+        }
+    }
+
+    private static boolean isExitCommand(String exitPoint) {
+        if ("exit".equals(exitPoint.toLowerCase())) {
+            System.out.println("Exiting");
+            return true;
+        }
+        return false;
+    }
+
+    private static String[] insertSort(String[] array) {
+        String temp;
+        int insertIndex;
+
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i].length() > array[i + 1].length()) {
+                temp = array[i + 1];
+                array[i + 1] = array[i];
+                insertIndex = i;
+                while (insertIndex > 0 && temp.length() < array[insertIndex - 1].length()) {
+                    array[insertIndex] = array[insertIndex - 1];
+                    insertIndex--;
+                }
+                array[insertIndex] = temp;
+            } else if (array[i].length() == array[i + 1].length()) {
+                if (array[i].compareTo(array[i + 1]) > 0) {
+                    temp = array[i + 1];
+                    array[i + 1] = array[i];
+                    insertIndex = i;
+                    while (insertIndex > 0 && array[insertIndex].compareTo(temp) > 0) {
+                        array[insertIndex] = array[insertIndex - 1];
+                        insertIndex--;
+                    }
+                    array[insertIndex] = temp;
+                }
+            }
+        }
+        return array;
     }
 }
+
