a3272d2d026f5bc460e3e7b994da58baaa2f86b1
mrPTqp <mrPTqp@gmail.com>
2019-07-13 10:05:04 +0300

    Solve task4

--- src/main/java/com/epam/homework/Task4.java
+++ src/main/java/com/epam/homework/Task4.java
@@ -1,5 +1,10 @@
 package com.epam.homework;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class Task4 {
 
     /**
@@ -25,9 +30,70 @@ public class Task4 {
      * Выходные данные:
      * a
      */
-    public static void main(String[] args) {
-        // TODO реализация
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String numberOfLinesString = br.readLine();
+        if (isExitCommand(numberOfLinesString)) {
+            return;
+        }
 
-        // TODO System.out.println(wordWithMinimalNumDiffLetters);
+        int numberOfLines = Integer.valueOf(numberOfLinesString);
+        String currentLine = br.readLine();
+        if (isExitCommand(currentLine)) {
+            return;
+        }
+
+        String[] inputLinesArr = currentLine.split(" ", numberOfLines);
+        String wordWithMinimalNumDiffLetters = getWordWithMinimalNumDiffLetters(inputLinesArr);
+        System.out.println(wordWithMinimalNumDiffLetters);
+    }
+
+    private static boolean isExitCommand(String exitPoint) {
+        if ("exit".equals(exitPoint.toLowerCase())) {
+            System.out.println("Exiting");
+            return true;
+        }
+        return false;
+    }
+
+    private static String getWordWithMinimalNumDiffLetters(String[] inputLinesArr) {
+        int minIndex = 0;
+        int minLength = 99999;
+
+        for (int i = 0; i < inputLinesArr.length; i++) {
+            char[] currentWordToArray = inputLinesArr[i].toCharArray();
+
+            for (int j = 0; j < currentWordToArray.length - 1; j++) {
+                for (int k = j + 1; k < currentWordToArray.length; k++) {
+                    if (currentWordToArray[j] == currentWordToArray[k]) {
+                        currentWordToArray[k] = 0;
+                    }
+                }
+            }
+
+            int countDifferentLetter = 0;
+
+            for (int j = 0; j < currentWordToArray.length; j++) {
+                if (currentWordToArray[j] != 0) {
+                    countDifferentLetter += 1;
+                }
+            }
+
+            char[] differentsLetterArray = new char[countDifferentLetter];
+            int count = 0;
+
+            for (int j = 0; j < currentWordToArray.length; j++) {
+                if (currentWordToArray[j] != 0) {
+                    differentsLetterArray[count] = currentWordToArray[j];
+                    count += 1;
+                }
+            }
+
+            if (differentsLetterArray.length < minLength) {
+                minLength = differentsLetterArray.length;
+                minIndex = i;
+            }
+        }
+        return inputLinesArr[minIndex];
     }
 }
