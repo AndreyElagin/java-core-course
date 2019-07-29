de878e0f4c10e63fe23f18c540d22c0285e715ca
mrPTqp <mrPTqp@gmail.com>
2019-07-29 14:01:36 +0300

    Group similar checks in method equals. Inline variables. Reorder redefined methods. Change names of tests. Add test for co-contract methods equals and hashcode.

--- src/main/java/com/epam/homework/external/equalshashcode/ComplexNumber.java
+++ src/main/java/com/epam/homework/external/equalshashcode/ComplexNumber.java
@@ -6,38 +6,6 @@ public final class ComplexNumber {
     private final double re;
     private final double im;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null) {
-            return false;
-        }
-        if (getClass() != o.getClass()) {
-            return false;
-        }
-        ComplexNumber other = (ComplexNumber) o;
-        if (re != other.re) {
-            return false;
-        }
-        if (im != other.im) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        int hashRe = Double.hashCode(re);
-        int hashIm = Double.hashCode(im);
-        result = prime * result + hashRe;
-        result = prime * result + hashIm;
-        return result;
-    }
-
     public ComplexNumber(double re, double im) {
         this.re = re;
         this.im = im;
@@ -49,5 +17,28 @@ public final class ComplexNumber {
 
     public double getIm() {
         return im;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ComplexNumber other = (ComplexNumber) o;
+        if (re != other.re || im != other.im) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 1;
+        result = 31 * result + Double.hashCode(re);
+        result = 31 * result + Double.hashCode(im);
+        return result;
     }
 }
--- src/test/java/com/epam/homework/external/equalshashcode/ComplexNumberTest.java
+++ src/test/java/com/epam/homework/external/equalshashcode/ComplexNumberTest.java
@@ -6,57 +6,77 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ComplexNumberTest {
-    ComplexNumber a = new ComplexNumber(1, 1);
-    ComplexNumber b = new ComplexNumber(1, 1);
-    ComplexNumber c = new ComplexNumber(1, 1);
-    Object d = a;
-    ComplexNumber e = new ComplexNumber(2, 2);
-
     @Test
-    @DisplayName("Reflexivity of method equals()")
+    @DisplayName("Equals method should be reflexive")
     void equals1() {
+        ComplexNumber a = new ComplexNumber(1, 1);
         assertTrue(a.equals(a));
     }
 
     @Test
-    @DisplayName("Symmetry of method equals()")
+    @DisplayName("Equals method should be symmetry")
     void equals2() {
+        ComplexNumber a = new ComplexNumber(1, 1);
+        ComplexNumber b = new ComplexNumber(1, 1);
         assertTrue(a.equals(b) && b.equals(a));
     }
 
     @Test
-    @DisplayName("Transitivity of method equals()")
+    @DisplayName("Equals method should be transitivity")
     void equals3() {
+        ComplexNumber a = new ComplexNumber(1, 1);
+        ComplexNumber b = new ComplexNumber(1, 1);
+        ComplexNumber c = new ComplexNumber(1, 1);
         assertTrue(a.equals(b) && b.equals(c) && c.equals(a));
     }
 
     @Test
-    @DisplayName("Consistency of method equals()")
+    @DisplayName("Equals method should be сonsistent")
     void equals4() {
+        ComplexNumber a = new ComplexNumber(1, 1);
+        ComplexNumber b = new ComplexNumber(1, 1);
         assertTrue(a.equals(b) == a.equals(b));
     }
 
     @Test
-    @DisplayName("Null comparison of method equals()")
+    @DisplayName("Null comparison of method equals")
     void equals5() {
+        ComplexNumber a = new ComplexNumber(1, 1);
         assertFalse(a.equals(null));
     }
 
     @Test
-    @DisplayName("Equality of hash-codes called on the one object")
+    @DisplayName("Hash codes calculated for a single object should be equal")
     void hashCode1() {
+        ComplexNumber a = new ComplexNumber(1, 1);
         assertTrue(a.hashCode() == a.hashCode());
     }
 
     @Test
-    @DisplayName("Equality of hash-codes called on the two same objects")
+    @DisplayName("Hash codes calculated for two identical objects should be equal")
     void hashCode2() {
+        ComplexNumber a = new ComplexNumber(1, 1);
+        Object d = a;
         assertTrue(a.hashCode() == d.hashCode());
     }
 
     @Test
-    @DisplayName("Equality of hash-codes called on the two different objects")
+    @DisplayName("Hash codes calculated for two different objects should be different")
     void hashCode3() {
+        ComplexNumber a = new ComplexNumber(1, 1);
+        ComplexNumber e = new ComplexNumber(2, 2);
         assertFalse(a.hashCode() == e.hashCode());
+    }
+
+    @Test
+    @DisplayName("Hash codes calculated for two identical objects should be equal")
+    void equalsAndHashCode() {
+        int min = 0;
+        int max = 100;
+        int re = (int) (min - 0.5 + Math.random() * (max - min + 1));
+        int im = (int) (min - 0.5 + Math.random() * (max - min + 1));
+        ComplexNumber a = new ComplexNumber(re, im);
+        ComplexNumber b = new ComplexNumber(re, im);
+        assertTrue(a.hashCode() == b.hashCode());
     }
 }
