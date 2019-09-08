44820945b5142d4b3ea4a2eeb389e06829e9c656
mrPTqp <mrPTqp@gmail.com>
2019-09-08 20:00:08 +0300

    first

---
+++ src/main/java/com/epam/homework/external/arraylist/MyArrayList.java
@@ -0,0 +1,37 @@
+package com.epam.homework.external.arraylist;
+
+public class MyArrayList<E> implements MyList<E> {
+
+
+
+
+    @Override
+    public void add(E e) {
+        E[] array = new E[10];
+    }
+
+    @Override
+    public E remove(int index) {
+        return null;
+    }
+
+    @Override
+    public boolean contains(E e) {
+        return false;
+    }
+
+    @Override
+    public E get(int index) {
+        return null;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
